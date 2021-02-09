b04fb2cbbaa6b17f293c91164ee1546dedd23cc4
neocaos <58259613+neocaos@users.noreply.github.com>
2021-02-09 09:29:53 +0100

    Update getAemetAPI.py

--- getAemetAPI.py
+++ getAemetAPI.py
@@ -1,6 +1,6 @@
 
 def getAPI(pwd):
-    if pwd == "J@zzinto13":
-        return "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqYXZpZXJyaXZhc2VzQGdtYWlsLmNvbSIsImp0aSI6ImNmNjQ2ZWIwLThmNDQtNDllZC04YzQxLWVjZThjY2I2ODJkYSIsImlzcyI6IkFFTUVUIiwiaWF0IjoxNTgyNzMyNzc5LCJ1c2VySWQiOiJjZjY0NmViMC04ZjQ0LTQ5ZWQtOGM0MS1lY2U4Y2NiNjgyZGEiLCJyb2xlIjoiIn0.USNStiBfYxOdMYm2VE5WKtnNTR-TJKV0RVnqXn6JFyA"
+    if pwd == "yourPassword":
+        return "your AEMET API"
     else:
         raise Exception("No tienes acceso a la API Key")
